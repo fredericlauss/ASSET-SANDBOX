a471fb6c537b2fbe4544c672aa87e8d72e89e54c
frederic <flausson@normandiewebschool.fr>
2025-02-08 09:08:49 +0100

    rotate

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -3,13 +3,20 @@ using System;
 
 public partial class Main : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    private Node3D niceOne;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
-	}
+        niceOne = GetNode<Node3D>("map/nice_one");
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	}
+        Animate();
+    }
+    private void Animate()
+    {
+        niceOne.RotationDegrees = new Vector3(45.0f, 0, 0);
+    }
 }
