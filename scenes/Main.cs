508ca485a6639517ac7f788ee2cd146069650910
frederic <flausson@normandiewebschool.fr>
2025-03-23 08:57:55 +0100

    change rotation speed

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -25,9 +25,9 @@ public partial class Main : Node
 
     private void Animate(double delta)
     {
-        niceOne.RotateX(Mathf.DegToRad(90.0f * (float)delta));
-        niceOne.RotateY(Mathf.DegToRad(90.0f * (float)delta));
-        niceOne.RotateZ(Mathf.DegToRad(90.0f * (float)delta));
+        niceOne.RotateX(Mathf.DegToRad(45.0f * (float)delta));
+        niceOne.RotateY(Mathf.DegToRad(45.0f * (float)delta));
+        niceOne.RotateZ(Mathf.DegToRad(45.0f * (float)delta));
 
         float rotationSpeed = 15.0f * (float)delta;
         if (!isReversing)
